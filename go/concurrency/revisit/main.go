caecb7520c7407266a1eb18eb459f6399889dc36
Gabriel Mazieri <mazierig.tech@gmail.com>
2025-12-10 19:35:37 -0300

    concurrent go

--- go/concurrency/revisit/main.go
+++ go/concurrency/revisit/main.go
@@ -1,34 +1,83 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"time"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-func main() {
-	team1 := "Palmeiras"
-	team2 := "Flamengo"
+var db *sql.DB
 
-	now := time.Now()
-	newRange := now.Unix()
-	fmt.Printf("%v\n", newRange)
-	fmt.Printf("%T\n", newRange)
+func dbConnection() {
+	var err error
+	db, err = sql.Open("sqlite3", "./concurrency.db")
+	if err != nil {
+		log.Fatal(err)
+	}
 	
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Ping failed %v", err)
+	}
+	log.Println("Database connected successfully")
+}
 
-	go func() {
-		fmt.Println("Goroutine start")
-		for range newRange {
-			fmt.Printf("%s", team1)
-		}
-		fmt.Println("Goroutine end")
-	}()
+func main() {
+	dbConnection()
 
-	go func() {
-		fmt.Println("Goroutine start")
-		for range newRange {
-			fmt.Printf("%s", team2)
+	// query := `SELECT id, first_name, last_name, random_number FROM users WHERE id = ?`
+	query := `SELECT id, first_name, last_name, random_number FROM Users;`
+	// rows, err := db.Query(query)
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Fatalf("Query failed %v", err)
+	}
+	defer rows.Close()
+	// fmt.Printf("Query result: %v\n", rows)
+
+	// rows.Scan(map[string]any{
+	// 	&id,
+	// })
+
+	for rows.Next() {
+		var id int
+		var firstName, lastName string
+		var randomNumber int
+
+		err := rows.Scan(&id, &firstName, &lastName, &randomNumber)
+		if err != nil {
+			log.Fatalf("Row scan failed %v", err)
 		}
-		fmt.Println("Goroutine end")
-	}()
-	time.Sleep(3 * time.Second)
+		fmt.Printf("ID: %d, First Name: %s, Last Name: %s, Random Number: %d\n", id, firstName, lastName, randomNumber)
+	}
+
+	if err = rows.Err(); err != nil {
+		log.Fatalf("Rows iteration error %v", rows.Err())
+	}
+	// team1 := "Palmeiras"
+	// team2 := "Flamengo"
+
+	// now := time.Now()
+	// newRange := now.Unix()
+	// fmt.Printf("%v\n", newRange)
+	// fmt.Printf("%T\n", newRange)
+
+	// go func() {
+	// 	fmt.Println("Goroutine start")
+	// 	for range newRange {
+	// 		fmt.Printf("%s", team1)
+	// 	}
+	// 	fmt.Println("Goroutine end")
+	// }()
+
+	// go func() {
+	// 	fmt.Println("Goroutine start")
+	// 	for range newRange {
+	// 		fmt.Printf("%s", team2)
+	// 	}
+	// 	fmt.Println("Goroutine end")
+	// }()
+	// time.Sleep(3 * time.Second)
 }
